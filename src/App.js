4e3d1a5e7744f79e3a1a008a18e68a7653516da6
SamuelFecomercio <156712738+SamuelFecomercio@users.noreply.github.com>
2024-03-01 22:37:12 -0300

    Revert "update"
    This reverts commit 8c56e4b7670b4ba9c1b3c1d689d915d52f44eff4.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
+import NotFound from "./pages/NotFound/NotFound";
+import Config from "./pages/config/config";
+import Historico from "./pages/historico/historico";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/config" element={<Config />} />
+        <Route path="/historico" element={<Historico />} />
+        <Route path="/404" element={<NotFound />} />
       </Routes>
     </Router>
   );
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,18 +1,47 @@
 import "./home.css";
-import React from "react";
 
-import HeaderComponent from "../../components/header/HeaderComponent";
-import HomeComponent from "../../components/home/HomeComponent";
-import InvestimentoComponents from "../../components/investimentos/investimentoComponents";
+import Cabecalho from "../../components/home/Cabecalho";
+import Pesquisa from "../../components/home/Pesquisa";
+import PesquisaVoz from "../../components/home/PesquisaVoz";
+import CategoriaNome1 from "../../components/home/CategoriaNome1";
+import Carrossel1 from "../../components/home/Carrossel1";
+import CategoriaNome2 from "../../components/home/CategoriaNome2";
+import Carrossel2 from "../../components/home/Carrossel2";
+import CategoriaNome3 from "../../components/home/CategoriaNome3";
+import Carrossel3 from "../../components/home/Carrossel3";
+import { useState } from "react";
+import Title from "../../components/Title/Title";
+import Carrossel from "../../components/Carrossel/Carrossel";
+import Navbar from "../../components/home/Navbar";
 
 function Home() {
-    return (
-        <div className="content-home"> 
-            <HeaderComponent />
-            <HomeComponent />
-            <InvestimentoComponents />
-        </div>
-    );
+  const [remedysState, setRemedysState] = useState(null);
+
+  return (
+    <main className="principal">
+      <Cabecalho />
+      <Pesquisa remedysState={remedysState} setRemedysState={setRemedysState} />
+      <PesquisaVoz />
+      <Navbar />
+
+      {remedysState ? (
+        <>
+          <Title categoria={"Medicamentos Ativos"} />
+          <Carrossel
+            show={"ativo"}
+            remedysState={remedysState}
+            setRemedysState={setRemedysState}
+          />
+          <Title categoria={"Medicamentos Inativos"} />
+          <Carrossel
+            show={"inativo"}
+            remedysState={remedysState}
+            setRemedysState={setRemedysState}
+          />
+        </>
+      ) : null}
+    </main>
+  );
 }
 
 export default Home;
