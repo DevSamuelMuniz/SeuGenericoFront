c4d65d1123979da30c40bf26dcb963bc6a1a1a2a
Samuel Muiniz <156712738+SamuelFecomercio@users.noreply.github.com>
2024-04-19 17:17:51 -0300

    remove carrossel

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,13 +2,7 @@ import "./home.css";
 
 import Cabecalho from "../../components/home/Cabecalho/Cabecalho";
 import Pesquisa from "../../components/home/Pesquisa/Pesquisa";
-import Categoria from "../../components/home/Categoria/Categoria";
-import Carrossel1 from "../../components/home/Carrossel1";
-import Carrossel2 from "../../components/home/Carrossel2";
-import Carrossel3 from "../../components/home/Carrossel3";
 import { useState } from "react";
-import Title from "../../components/Title/Title";
-import Carrossel from "../../components/Carrossel/Carrossel";
 import Navbar from "../../components/home/Navbar/Navbar";
 import SearchCategoria from "../../components/search/SearchCategoria/SearchCategoria";
 import MedicamentosList from "../../components/search/MedicamentosList/MedicamentosList";
