e9df553f758cebf43f95b81c0523f81ad1efc9b8
SamuelFecomercio <>
2024-11-27 16:36:20 -0300

    update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import {
   Navigate,
 } from "react-router-dom";
 import Home from "./pages/home/home";
-import NotFound from "./pages/NotFound/NotFound";
-import Config from "./pages/config/config";
-import ModoDeExibicao from "./pages/config-options/modo-de-exibicao/modo-de-exibicao";
-import IdiomaPage from "./pages/config-options/idioma/idiomaPage";
 import SplashScreen from "./pages/splashScreen/splashScreen.js";
 
 function App() {
@@ -29,10 +25,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/config" element={<Config />} />
-          <Route path="/Modo-de-exibição" element={<ModoDeExibicao />} />
-          <Route path="/Idioma" element={<IdiomaPage />} />
-          <Route path="/404" element={<NotFound />} />
         </Routes>
       )}
     </Router>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,7 +4,6 @@ import Cabecalho from "../../components/home/Cabecalho/Cabecalho";
 import Pesquisa from "../../components/home/Pesquisa/Pesquisa";
 import { useState } from "react";
 import Navbar from "../../components/home/Navbar/Navbar";
-import SearchCategoria from "../../components/search/SearchCategoria/SearchCategoria";
 import MedicamentosList from "../../components/search/MedicamentosList/MedicamentosList";
 
 function Home() {
@@ -14,19 +13,24 @@ function Home() {
   return (
     <main className="principal">
       <Cabecalho />
-      <Pesquisa setMedicamentoNome={setMedicamentoNome} medicamentoNome={medicamentoNome} remedysState={remedysState} setRemedysState={setRemedysState} />
-
+      <Pesquisa
+        setMedicamentoNome={setMedicamentoNome}
+        medicamentoNome={medicamentoNome}
+        remedysState={remedysState}
+        setRemedysState={setRemedysState}
+      />
 
       <Navbar setMedicamentoNome={setMedicamentoNome} />
 
       {remedysState ? (
         <>
-          <SearchCategoria/>
+          <div className="search-categoria-container">
+            <h1 className="search-categoria-nome">Medicametos encontrados</h1>
+          </div>
           <MedicamentosList
             remedysState={remedysState}
             setRemedysState={setRemedysState}
           />
-          
         </>
       ) : null}
     </main>
